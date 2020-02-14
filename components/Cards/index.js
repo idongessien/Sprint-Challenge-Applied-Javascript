a473eefda1dab5d6f2c549cbb7c328ec09a14f61
Idong Essien <idong.essien@comcast.net>
2020-02-14 13:53:15 -0500

    Article component created, ready to add to DOM

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios({
+    url: 'https://lambda-times-backend.herokuapp.com/articles',
+    method: 'get'
+}).then((response) => {
+    console.log(response.data);
+}).catch(err => console.error(err));
+
+
+function Articles(headline, authorPhoto, authorName) {
+    const articleCard = document.createElement('div');
+          cardHeadline = document.createElement('div');
+          author = document.createElement('div');
+          imgContainer = document.createElement('div');
+          img = document.createElement('img');
+          authName = document.createElement('span');
+    
+    articleCard.classList.add('card');
+    cardHeadline.classList.add('headline');
+    author.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    var cardsContainer = document.querySelector('.cards-container');
+
+    cardsContainer.appendChild(articleCard);
+    articleCard.appendChild(cardHeadline);
+    articleCard.appendChild(author);
+    articleCard.appendChild(imgContainer);
+    articleCard.appendChild(authName);
+    imgContainer.appendChild(img);
+
+    cardHeadline.innerHTML = headline;
+    img.src = authorPhoto;
+    authName.innerHTML = authorName;
+
+    return Articles;
+}
+
+Articles();
